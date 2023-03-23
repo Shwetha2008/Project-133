8429466d77cae253259b1f452896b48803b953f8
Shwetha <104769790+Shwetha2008@users.noreply.github.com>
2023-03-23 13:01:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function next()
+{
+    window.location = "Bed.html";
+}
+function next1()
+{
+    window.location = "vase.html";
+}
+function next2()
+{
+    window.location = "fruits.html";
+}
+function next3()
+{
+    window.location = "laptop.html";
+}
+function next4()
+{
+    window.location = "cup.html";
+}
